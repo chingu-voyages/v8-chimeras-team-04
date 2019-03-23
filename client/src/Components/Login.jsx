75f79e17f5e95f2f96e4b8604e4a9848df34d03a
Paul Hong <paulhong1124@gmail.com>
2019-03-23 09:24:36 -0700

    fix error

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,21 +1,20 @@
-// import React, { Component } from 'react'
 import React, { useState } from 'react';
 
 export default function Login() {
-  const [ username, setUsername ] = useState();
-  const [ password, setPassword ] = useState();
+  const [ username, setUsername ] = useState("");
+  const [ password, setPassword ] = useState("");
 
   return (
     <div className="login">
-      <LoginForm setUsername={setUsername} setPassword={setPassword} />
+      <LoginForm username={username} password={password} setUsername={setUsername} setPassword={setPassword} />
       {console.log(username, password)}
     </div>
   )
 }
 
-function LoginForm({ setUsername, setPassword }) {
-  const [ value, setValue ] = useState();
-  const [ pw, setPw ] = useState();
+function LoginForm({ username, setUsername, password, setPassword }) {
+  const [ value, setValue ] = useState("");
+  const [ pw, setPw ] = useState("");
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -44,66 +43,3 @@ function LoginForm({ setUsername, setPassword }) {
     </form>
   )
 }
-
-
-
-
-// export class Login extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       username: '',
-//       password: '',
-//     }
-//   }
-
-//   handleChange = e => {
-//     this.setState({
-//       [e.target.id]: e.target.value
-//     })
-//   }
-
-//   handleSubmit = e => {
-//     e.preventDefault();
-//     // Connect with DB
-//   }
-
-//   isNotBlank() {
-//     return this.state.username.length > 0 && this.state.password.length > 0;
-//   }
-
-//   render() {
-//     const { username, password } = this.state
-//     return (
-//       <div className="login">
-//         <form onSubmit={this.handleSubmit}>
-//           <label name="email">Email: </label>
-//           <input 
-//             id="username"
-//             type="text" 
-//             name="email" 
-//             value={username}
-//             onChange={this.handleChange}
-//           />
-//           <label name="password">Password: </label>
-//           <input 
-//             id="password"
-//             type="password" 
-//             name="password"
-//             value={password}
-//             onChange={this.handleChange}
-//           />
-//           <button 
-//             type="submit"
-//             disabled={!this.isNotBlank()}
-//           >
-//             Login
-//           </button>
-//           <button>Sign Up</button>
-//         </form>
-//       </div>
-//     )
-//   }
-// }
-
-// export default Login;
