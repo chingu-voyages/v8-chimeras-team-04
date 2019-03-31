dbd276c0f0f58b9748e79f293c6a1bc3562e01f8
Carlos Fins <clockwerkz@gmail.com>
2019-03-30 21:01:34 -0700

    adding in post for new apps

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 
 const { authenticate } = require('./middleware/authenticate');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json()); //sets up bodyParser as middlewear
 
 const data = {  
   '1': {
@@ -30,7 +34,7 @@ const data = {
 
 }
 
-const app = express();
+let currentIndex = 5;
 
 
 app.get('/chimeras', (req, res) => {
@@ -48,7 +52,12 @@ app.get('/getAllApps', (req,res) => {
 
 app.get('/app/:id', (req, res)=> {
   const app = data[req.params.id];
-  
+});
+
+app.post('/addApp', (req, res)=>{
+  const newId = currentIndex++;
+  console.log(req.body);
+  res.send(req.body);
 });
 
 const PORT = 5000;
