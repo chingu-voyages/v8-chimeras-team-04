e878ab5e0dc2ef9f90ba9ae8c8cd8a539ebe0190
Natalie Roode <nroode@gmail.com>
2019-04-04 21:36:45 -0700

    add status and notes placeholders

--- client/src/components/FullApp/FullApp.jsx
+++ client/src/components/FullApp/FullApp.jsx
@@ -5,7 +5,7 @@ import './FullApp.scss';
 export default function FullApp() {
 
     return (
-        <div>
+        <div className="app-expanded">
             <ul className="app-status-list">
                 <li>Submitted</li>
                 <li>Code Challenge</li>
@@ -14,8 +14,9 @@ export default function FullApp() {
                 <li>Interview 2</li>
                 <li>Received Offer</li>
             </ul>
-            <div>
-                <h2>Notes</h2>
+            <div className="app-notes">
+                <h3 className="app-notes-title">Notes</h3>
+                <p className="app-notes-content">This is where the notes would go if we decided to include this in our app.  Doesn't it look nice?</p>
             </div>
         </div>
     )
