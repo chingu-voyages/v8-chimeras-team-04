6c8637524101d0ad33f29bfd478ed531c8a79e40
Terrance Xin <xin.terrance@gmail.com>
2019-04-18 23:01:51 -0700

    Connect modal with db

--- app.js
+++ app.js
@@ -6,25 +6,25 @@ const mongoose = require('mongoose');
 const router = require('./server/router');
 const app = express();
 
-const mongoUri = process.env.MONGO_URI || 'mongodb+srv://chingu:chingu@job-app-cluster-krb6h.mongodb.net/test?retryWrites=true';
+const mongoUri =
+	process.env.MONGO_URI || 'mongodb+srv://chingu:chingu@job-app-cluster-krb6h.mongodb.net/test?retryWrites=true';
 const options = {
-  reconnectTries: 5,
-  poolSize: 10,
-  useNewUrlParser: true,
+	reconnectTries: 5,
+	poolSize: 10,
+	useNewUrlParser: true,
 };
 
 mongoose.connect(mongoUri, options, () => {
-  // mongoose.connection.db.dropDatabase();
+	// mongoose.connection.db.dropDatabase();
 });
-mongoose.set('useCreateIndex', true)
+mongoose.set('useCreateIndex', true);
 mongoose.set('debug', true);
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.static('public'));
 router(app);
 
-
-const PORT = process.env.PORT || 5000;;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
