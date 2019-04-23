da43ce9b6de4e0cbafbae79a52e6778d45cbae80
Paul Hong <paulhong1124@gmail.com>
2019-04-22 21:50:29 -0700

    updates

--- client/src/Components/Login/index.jsx
+++ client/src/Components/Login/index.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+
+import ok from '../../img/ok.svg';
+import jobHunt from '../../img/job_hunt.svg';
 import './login.scss';
 
 export default function Login({ auth, setAuth, setCurrentUser }) {
@@ -13,19 +16,33 @@ export default function Login({ auth, setAuth, setCurrentUser }) {
 		<div className="login">
 
 			<div className="login-landing-page">
-				<h2>Job Application Tracker</h2>
-				<p>Keep track of your job applications by creating an account</p>
+				<img src={jobHunt} className="login-landing-page-main" />
+				<div className="login-landing-page-sub">
+					<img src={ok} />
+					<p>Create a FREE account to easily keep track of your job applications.</p>
+				</div>
+				<div className="login-landing-page-sub">
+					<img src={ok} />
+					<p>Track your rate of success at each stage in the process to identify areas to focus to imporve success.</p>
+				</div>
 			</div>
 
 
 			<div className="login-forms">
-				<div className="login-btns">
+				{/* <div className="login-btns">
 						<button onClick={handleClick('login')}>Log In</button>
 						<button onClick={handleClick('signup')}>Sign Up</button>
 
-				</div>
+				</div> */}
+
+				<h2 className="login-forms-heading">
+					{
+						login === 'login' ? "Log In" : "Sign Up"
+					}
+				</h2>
+
 				<form onSubmit={handleSubmit}>
-					<label htmlFor="username">Username: </label>
+					<label htmlFor="username">Username </label>
 					<input
 						type="text"
 						name="username"
@@ -33,9 +50,9 @@ export default function Login({ auth, setAuth, setCurrentUser }) {
 						onChange={e => setUsername(e.target.value)}
 						autoComplete={false}
 					/>
-					<label htmlFor="password">Password: </label>
+					<label htmlFor="password">Password </label>
 					<input type="password" name="password" value={password} onChange={e => setPassword(e.target.value)} />
-					{login === 'signup' && <label htmlFor="confirm">Confirm Password: </label>}
+					{login === 'signup' && <label htmlFor="confirm">Confirm Password </label>}
 					{login === 'signup' && (
 						<input
 							type="password"
@@ -44,7 +61,18 @@ export default function Login({ auth, setAuth, setCurrentUser }) {
 							onChange={e => setPasswordConfirm(e.target.value)}
 						/>
 					)}
+					{
+						login === 'login' ? 
+						<button className="redirect" onClick={handleClick('signup')}>
+							Don't have an account yet? Sign up for FREE
+						</button>
+						:
+						<button className="redirect" onClick={handleClick('login')}>
+							Already have an account? Sign in here
+						</button>
+					}
 					<span>{error}</span>
+					<br/>
 					<input type="submit" value="Submit" />
 				</form>
 			</div>
