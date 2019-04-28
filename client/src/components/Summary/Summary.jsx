fb1528320db69d331721276cf3c478d3e106234a
Natalie Roode <nroode@gmail.com>
2019-04-28 13:18:00 -0700

    added image and adjust spacing

--- client/src/components/Summary/Summary.jsx
+++ client/src/components/Summary/Summary.jsx
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
 
 import './Summary.scss';
+import codeImg from '../img/dev_activity.svg';
 
 export default function Summary() {
-  
-  return (
-    <div className="summary-container">
-        <h1 className="summary-title">Summary</h1>
-        <div>
-            <p>stats will go here</p>
+
+    return (
+        <div className="summary-container">
+            <h1 className="summary-title">Summary</h1>
+            <div className="summary-content">
+                <div>
+                    <p>stats will go here</p>
+                </div>
+                <div className='dev-img-container'>
+                    <img src={codeImg} className="dev-img"></img>
+                </div>
+            </div>
         </div>
-        image will go here
-    </div>
-  )
+    )
 }
